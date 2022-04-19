55b1f94cb5a6e623e52f9105ceaedc9398ea048d
Salvare000 <k020g1393@g.neec.ac.jp>
2022-04-19 10:17:52 +0900

    リポジトリ作成に関すること

---
+++ sln/FbxLoader.cpp
@@ -0,0 +1,58 @@
+﻿#include "FbxLoader.h"
+#include <cassert>
+
+//静的メンバ変数の実態
+const std::string FbxLoader::baseDirectory = "Resources/";
+
+
+FbxLoader* FbxLoader::GetInstance()
+{
+    static FbxLoader instance;
+    return &instance;
+}
+
+void FbxLoader::Initialize(ID3D12Device* device)
+{
+    //再初期化チェック
+    assert(fbxManager == nullptr);
+    //引数からメンバ変数に代入
+    this->device = device;
+    //fbxマネージャーの生成
+    fbxManager = FbxManager::Create();
+    //Fbxマネージャの入出力設定
+    FbxIOSettings* ios = FbxIOSettings::Create(fbxManager, IOSROOT);
+    fbxManager->SetIOSettings(ios);
+    //fbxインポータの生成
+    fbxImporter = FbxImporter::Create(fbxManager, "");
+}
+
+void FbxLoader::Finalize()
+{
+    //各種FBXインスタンス破棄
+    fbxImporter->Destroy();
+    fbxManager->Destroy();
+}
+
+void FbxLoader::LoadModelFromFile(const string& modelName)
+{
+    //モデルと同じ名前のフォルダから読み込み
+    const string directoryPath = baseDirectory +
+        modelName + "/";
+    //拡張子.fbx追加
+    const string fileName = modelName + ".fbx";
+    //連結してフルパスを得る
+    const string fullpath = directoryPath + fileName;
+
+    //ファイル名指定、FBXファイル読み込み
+    if (!fbxImporter->Initialize(fullpath.c_str(),
+        -1, fbxManager->GetIOSettings())) {
+        assert(0);
+    }
+
+    //シーン生成
+    FbxScene* fbxScene =
+    FbxScene::Create(fbxManager, "fbxScene");
+
+    //ファイルからロードしたFBXの情報をシーンにインポート
+    fbxImporter->Import(fbxScene);
+}
---
+++ sln/FbxLoader.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "fbxsdk.h"
+
+#include <d3d12.h>
+#include <d3dx12.h>
+
+#include <string>
+
+class FbxLoader
+{
+private:
+	//std省略
+	using string = std::string;
+
+public:
+	/// <summary>
+	/// シングルトンインスタンスの取得
+	/// </summary>
+	/// <returns>インスタンス</returns>
+	static FbxLoader* GetInstance();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="device"></param>
+	void Initialize(ID3D12Device* device);
+
+	/// <summary>
+	/// 後始末
+	/// </summary>
+	void Finalize();
+
+	/// <summary>
+/// ファイルからFBXモデル読み込み
+/// </summary>
+	void LoadModelFromFile(const string& modelName);
+
+private:
+	// privateなコンストラクタ（シングルトンパターン）
+	FbxLoader() = default;
+	// privateなデストラクタ（シングルトンパターン）
+	~FbxLoader() = default;
+	// コピーコンストラクタを禁止（シングルトンパターン）
+	FbxLoader(const FbxLoader& obj) = delete;
+	// コピー代入演算子を禁止（シングルトンパターン）
+	void operator=(const FbxLoader& obj) = delete;
+
+	//d3d12デバイス
+	ID3D12Device* device = nullptr;
+	//fbxマネージャー
+	FbxManager* fbxManager = nullptr;
+	//fbxインポータ
+	FbxImporter* fbxImporter = nullptr;
+
+public:
+
+	//モデル格納ルートパス
+	static const string baseDirectory;
+
+
+};
