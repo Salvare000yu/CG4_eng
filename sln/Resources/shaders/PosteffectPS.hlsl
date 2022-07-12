4dd4ec82bd029a176094d4ddfa34f7aec6618f8e
Salvare000 <k020g1393@g.neec.ac.jp>
2022-07-12 13:57:12 +0900

    ポストエフェクト　ぼかし

--- sln/Resources/shaders/PosteffectPS.hlsl
+++ sln/Resources/shaders/PosteffectPS.hlsl
@@ -1,19 +1,44 @@
 #include "PostEffect.hlsli"
 
-Texture2D<float4> tex0 : register(t0);   // 0番スロットに設定されたテクスチャ
+Texture2D<float4> tex0 : register(t0); // 0番スロットに設定されたテクスチャ
 Texture2D<float4> tex1 : register(t1);
-SamplerState smp : register(s0);        // 0番スロットに設定されたサンプラー
+SamplerState smp : register(s0); // 0番スロットに設定されたサンプラー
+
+float Gaussian(float2 drawUV, float2 pickUV, float sigma)
+{
+	float d = distance(drawUV, pickUV);
+	return exp(-(d * d) / (2 * sigma * sigma));
+}
 
 float4 main(VSOutput input) : SV_TARGET
 {
-float4 texcolor0 = tex0.Sample(smp, input.uv);
-	float4 texcolor1 = tex1.Sample(smp, input.uv);
+//float4 texcolor0 = tex0.Sample(smp, input.uv);
+//	float4 texcolor1 = tex1.Sample(smp, input.uv);
+//
+//	float4 color = texcolor0;
+//	if (fmod(input.uv.y, 0.1f) < 0.05f)
+//	{
+//		color = texcolor1;
+//	}
+//
+//	return float4(color.rgb, 1);
 
-	float4 color = texcolor0;
-	if (fmod(input.uv.y, 0.1f) < 0.05f)
+	//ぼかし２↓
+	float totalWeight = 0, _Sigma = 0.005, _StepWidth = 0.001;
+	float4 col = float4(0, 0, 0, 0);
+
+	for (float py = -_Sigma * 2; py <= _Sigma * 2; py += _StepWidth)
 	{
-		color = texcolor1;
+		for (float px = -_Sigma * 2; px <= _Sigma * 2; px += _StepWidth)
+		{
+            float2 pickUV = input.uv + float2(px, py);
+            float weight = Gaussian(input.uv, pickUV, _Sigma);
+			col += tex0.Sample(smp, pickUV) * weight;
+			
+			totalWeight += weight;
+		}
 	}
 
-	return float4(color.rgb, 1);
+	col.rgb = col.rgb / totalWeight;
+	return col;
 }
